44186d68fc350bd9a156e47375db4566f89d1f65
Stephen <stephennguyen24@gmail.com>
2025-11-16 14:54:28 +0700

    feat: add quick menu item links

--- src/components/boost-info.js
+++ src/components/boost-info.js
@@ -36,7 +36,7 @@ export default function renderBoostInfo({
   </div>`
        : ""
    }
-   ${renderQuickAppMenu()}
+   ${renderQuickAppMenu(shopURLWithoutDomain)}
     </div>
    ${renderHighlightToggle()}
   </div>
--- src/components/quick-app-menu.js
+++ src/components/quick-app-menu.js
@@ -1,53 +1,49 @@
 import svgLibrary from "../assets/svgs/svgLibrary.js";
 
-const menuItems = [
-  {
-    name: "Home",
-    tooltip: "Home",
-    svgPath: svgLibrary.appHomePage,
-  },
-  {
-    name: "Filter",
-    tooltip: "Filter",
-    svgPath: svgLibrary.appFilter,
-  },
-  {
-    name: "Search",
-    tooltip: "Search",
-    svgPath: svgLibrary.appSearch,
-  },
-  {
-    name: "Merchandising",
-    tooltip: "Merchandising",
-    svgPath: svgLibrary.appMerchandising,
-  },
-  {
-    name: "Integrations",
-    tooltip: "Integrations",
-    svgPath: svgLibrary.appIntegrations,
-  },
-  {
-    name: "Settings",
-    tooltip: "Settings",
-    svgPath: svgLibrary.appSettings,
-  },
-];
+export default function renderQuickAppMenu(shopURLWithoutDomain) {
+  const baseURL = `https://admin.shopify.com/store/${shopURLWithoutDomain}/apps/product-filter-search`;
+  const menuItems = [
+    {
+      name: "Home",
+      tooltip: "Home",
+      svgPath: svgLibrary.appHomePage,
+      urlPath: "",
+    },
+    {
+      name: "Filter",
+      tooltip: "Filter",
+      svgPath: svgLibrary.appFilter,
+      urlPath: "/filter-tree",
+    },
+    {
+      name: "Search",
+      tooltip: "Search",
+      svgPath: svgLibrary.appSearch,
+      urlPath: "/search/standard-search/relevance-settings",
+    },
+    {
+      name: "Merchandising",
+      tooltip: "Merchandising",
+      svgPath: svgLibrary.appMerchandising,
+      urlPath: "/merchandising",
+    },
+    {
+      name: "Integrations",
+      tooltip: "Integrations",
+      svgPath: svgLibrary.appIntegrations,
+      urlPath: "/shopify-integration",
+    },
+    {
+      name: "Settings",
+      tooltip: "Settings",
+      svgPath: svgLibrary.appSettings,
+      urlPath: "/settings",
+    },
+  ];
 
-// Create a menu item component
-function createMenuItem(item) {
-  return `
-                <div class="icon-item" data-name="${item.name}">
-                    <svg viewBox="0 0 24 24">
-                        ${item.svgPath}
-                    </svg>
-                    <span class="item-tooltip"><span>${item.tooltip}</span></span>
-                </div>
-            `;
-}
-
-// Create the complete menu with container
-function createMenu(items) {
-  const menuItemsHTML = items.map((item) => createMenuItem(item)).join("");
+  const menuItemsHTML = menuItems
+    .map((item) => createMenuItem(item, baseURL))
+    .join("");
 
   return `
                 <div class="menu-container">
@@ -58,6 +54,20 @@ function createMenu(items) {
             `;
 }
 
-export default function renderQuickAppMenu() {
-  return createMenu(menuItems);
+// Create a menu item component
+function createMenuItem(item, baseURL) {
+  return `
+                <div class="icon-item" data-name="${item.name}">
+                <a class="item-link" href="${
+                  baseURL + item.urlPath
+                }" target="_blank">
+                    <svg viewBox="0 0 24 24">
+                        ${item.svgPath}
+                    </svg>
+                    <span class="item-tooltip"><span>${
+                      item.tooltip
+                    }</span></span>
+                </a>
+                </div>
+            `;
 }
