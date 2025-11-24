970f18fc433fd5c788f3be7fbf6309b5360d2b84
Stephen <stephennguyen24@gmail.com>
2025-11-24 10:16:45 +0700

    chore: refactor theme-info card logic

--- src/components/theme-info.js
+++ src/components/theme-info.js
@@ -1,6 +1,5 @@
 import svgLibrary from "../assets/svgs/svgLibrary.js";
 import * as Utils from "./utils.js";
-
 export default function renderThemeInfo({
   themeId,
   themeName,
@@ -13,49 +12,67 @@ export default function renderThemeInfo({
   const themeSchemaInfo = themeSchema
     ? `${themeSchema}${themeSchemaVersion ? `_v${themeSchemaVersion}` : ""}`
     : "No Data";
+  const beaconClass = isLive ? "live-theme-beacon" : "unpublished-theme-beacon";
+  const beaconStatusText = isLive ? "Live" : "Draft";
+
+  const themeCodeEditorLink =
+    shopURLWithoutDomain && themeId
+      ? `https://admin.shopify.com/store/${shopURLWithoutDomain}/themes/${themeId}`
+      : "#";
   const previewLink = Utils.buildPreviewLink(windowLocation, themeId);
   const themeEditorLink = Utils.buildThemeEditorLink(
     windowLocation,
     shopURLWithoutDomain,
     themeId
   );
-  const themeCodeEditorLink = `https://admin.shopify.com/store/${shopURLWithoutDomain}/themes/${themeId}`;
 
-  //Add live beacon
+  const renderActionButtons = () => {
+    if (!themeId) return "";
+
+    return `
+      <div class="theme-action-buttons">
+        ${Utils.renderButtonLink(
+          svgLibrary.codeEditor,
+          "",
+          themeCodeEditorLink,
+          "theme-code-editor"
+        )}
+        ${Utils.renderButtonLink(
+          svgLibrary.editor,
+          "",
+          themeEditorLink,
+          "theme-editor"
+        )}
+      </div>
+    `;
+  };
   return `<div class="section-content theme-info">
-    <div class="beacon-wrapper ${
-      isLive ? "live-theme-beacon" : "unpublished-theme-beacon"
-    }">
+    <div class="beacon-wrapper ${beaconClass}">
       <div class="beacon"></div>
-        <div class="beacon-status">${isLive ? "Live" : "Draft"}</div>
+      <div class="beacon-status">${beaconStatusText}</div>
     </div>
+    
     <div class="theme-action-wrapper">
-    <div class="theme-action-buttons">
-  ${Utils.renderButtonLink(
-    svgLibrary.codeEditor,
-    "",
-    themeCodeEditorLink,
-    "theme-code-editor"
-  )}
-  ${Utils.renderButtonLink(
-    svgLibrary.editor,
-    "",
-    themeEditorLink,
-    "theme-editor"
-  )}
-  </div>
-  ${Utils.renderCopyableField("", "Preview Link", previewLink, "preview-link")}
-  </div>
-  ${Utils.renderCopyableField("", themeName, undefined, "theme-name")}
-  <hr class="divider"/>
-  <div class="theme-extra-info">
-    ${Utils.renderCopyableField("ID", themeId, undefined, "theme-id")}
-    ${Utils.renderCopyableField(
-      "Schema",
-      themeSchemaInfo,
-      undefined,
-      "theme-schema"
-    )}
-  </div>
+      ${renderActionButtons()}
+      ${Utils.renderCopyableField(
+        "",
+        "Preview Link",
+        previewLink,
+        "preview-link"
+      )}
+    </div>
+    ${Utils.renderCopyableField("", themeName, undefined, "theme-name")}
+    
+    <hr class="divider"/>
+    
+    <div class="theme-extra-info">
+      ${Utils.renderCopyableField("ID", themeId, undefined, "theme-id")}
+      ${Utils.renderCopyableField(
+        "Schema",
+        themeSchemaInfo,
+        undefined,
+        "theme-schema"
+      )}
+    </div>
   </div>`;
 }
